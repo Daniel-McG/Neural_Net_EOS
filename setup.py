9022ed30f4270b9cb21aad432b40c7d4d6d0bfe2
Daniel-McG <rileyday@tutanota.com>
2023-10-02 10:02:43 +0100

    Modified architechture to stop oscillating loss

--- setup.py
+++ setup.py
@@ -28,36 +28,32 @@ class BasicLightning(pl.LightningModule):
           nn.Tanh(),
           nn.Linear(2,2),
           nn.Tanh(),
-          nn.Linear(2,2),
-          nn.Tanh()
+        #   nn.Linear(4,2),
+        #   nn.Tanh()
         )
     def forward(self,x):
         out = self.s1(x)
         return out
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(),lr = 0.001 )
+        return torch.optim.SGD(self.parameters(),lr = 0.00000000000000001 )
     def training_step(self,train_batch,batch_index):
-
         input_i,target_i = train_batch            #Unpacking data from a batch
         output_i = self.forward(input_i)    #Putting input data frm the batch through the neural network
         loss = (output_i-target_i)**2       #Calculating loss
         mean_loss = torch.mean(loss)
-        print(mean_loss)
         self.log("train_loss",mean_loss)         #Logging the training loss
-        return {'train_loss': mean_loss}
+        return {'loss': mean_loss}
     
-    def validation_step(self, val_batch, batch_idx):
-        val_input_i, val_target_i = val_batch
-        val_output_i = self.forward(val_input_i)
-        loss = (val_output_i-val_target_i)**2
-        mean_loss = torch.mean(loss)
-        self.log("val_loss",mean_loss) 
-        return {"val_loss": mean_loss}
+    # def validation_step(self, val_batch, batch_idx):
+    #     val_input_i, val_target_i = val_batch
+    #     val_output_i = self.forward(val_input_i)
+    #     loss = (val_output_i-val_target_i)**2
+    #     mean_loss = torch.mean(loss)
+    #     self.log("val_loss",mean_loss) 
+    #     return {"val_loss": mean_loss}
 
 def train_func(config):
     # Read data from csv
-    data_df = pd.read_csv('/home/daniel/Downloads/MSc_data.csv')
-    # Create and load data into dataoader
     data_df = pd.read_csv('/home/daniel/Downloads/MSc_data.csv',names=['rho','T','P','U'])
     input_df = data_df[['rho','T']]
     target_df = data_df[['P','U']]
@@ -72,16 +68,16 @@ def train_func(config):
 
     dataset = TensorDataset(input_tensor,target_tensor)
 
-    train_tensor_size = int(len(input_tensor) * 0.8)
+    train_tensor_size = int(len(input_tensor) * 0.9)
     val_tensor_size = len(input_tensor) - train_tensor_size
     seed = torch.Generator().manual_seed(584)
     train_set, val_set = torch.utils.data.random_split(dataset, [train_tensor_size, val_tensor_size], generator=seed)
 
-    train_dataloader = DataLoader(train_set)
-    val_dataloader = DataLoader(val_set)
+    train_dataloader = DataLoader(train_set,batch_size=200)
+    val_dataloader = DataLoader(val_set,batch_size=200)
     model = BasicLightning()
     trainer = pl.Trainer(
-        max_epochs=100000,
+        max_epochs=50000,
         devices="auto",
         accelerator="auto",
         strategy=ray.train.lightning.RayDDPStrategy(),
