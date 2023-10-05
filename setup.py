76144d18f325709f772bee90fab0412fc84ebb5b
Daniel-McG <rileyday@tutanota.com>
2023-10-05 12:47:00 +0100

    Added plotting of data, removed deprecated code

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ import pandas as pd
 import torch.nn as nn
 import lightning as L
 import pytorch_lightning as pl
+import seaborn as sns
+import matplotlib.pyplot as plt
 from torch.utils.data import TensorDataset, DataLoader
 from lightning.pytorch.loggers import TensorBoardLogger
 from ray.train.torch import TorchTrainer
@@ -36,15 +38,12 @@ class BasicLightning(pl.LightningModule):
         out = self.s1(x)
         return out
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(),lr = 0.0001 )
+        return torch.optim.Adam(self.parameters(),lr = 0.000001 )
     def training_step(self,train_batch,batch_index):
         input_i,target_i = train_batch            #Unpacking data from a batch
         output_i = self.forward(input_i)    #Putting input data frm the batch through the neural network
         loss = (output_i-target_i)**2       #Calculating loss
         mean_loss = torch.mean(loss)
-        # self.log("input",input_i)
-        # self.log("target",target_i)
-        # self.log("output",output_i)
         self.log("train_loss",mean_loss)         #Logging the training loss
         return {"loss": mean_loss}
     def validation_step(self, val_batch, batch_idx):
@@ -58,10 +57,24 @@ class BasicLightning(pl.LightningModule):
 def train_func(config):
     # Read data from csv
     data_df = pd.read_csv('/home/daniel/Downloads/MSc_data.csv',names=['rho','T','P','U'])
+
+    #Preprocessing the data
     train_df,test_df = train_test_split(data_df,train_size=0.9)
     scaler = MinMaxScaler(feature_range =(0,1))
     train_arr= scaler.fit_transform(train_df)
     val_arr = scaler.transform(test_df)
+    for column in range(0,4,1):
+        plt.clf()
+        train_histplot = sns.histplot(data = train_arr[:,column])
+        train_fig = train_histplot.get_figure()
+        train_fig.savefig("/home/daniel/Pictures/BS_32_train_col{}.png".format(str(column)))
+
+    for column in range(0,4,1):
+        plt.clf()
+        val_histplot = sns.histplot(data = val_arr[:,column])
+        val_fig = val_histplot.get_figure()
+        val_fig.savefig("/home/daniel/Pictures/BS_32_val_col{}.png".format(str(column)))
+    #Splitting the preprocessed data into the inputs and targets
     train_inputs = torch.tensor(train_arr[:,[0,1]])
     train_targets = torch.tensor(train_arr[:,[2,3]])
     val_inputs = torch.tensor(val_arr[:,[0,1]])
@@ -71,35 +84,18 @@ def train_func(config):
     val_inputs = val_inputs.float()
     val_targets = val_targets.float()
     print(train_inputs)
+    # Loading inputs and targets into the dataloaders
     train_dataset = TensorDataset(train_inputs,train_targets)
     val_Dataset = TensorDataset(val_inputs,val_targets)
-    train_dataloader = DataLoader(train_dataset,batch_size = 200)
-    val_dataloader = DataLoader(val_Dataset,batch_size = 200)
-    # input_tensor = torch.tensor(input_values)
-    # target_tensor = torch.tensor(target_values)
-    # input_tensor = input_tensor.float()
-    # target_tensor = target_tensor.float()
-    # input_tensor = input_tensor.to(device)
-    # target_tensor = target_tensor.to(device)
-    # train
-    # dataset = TensorDataset(input_tensor,target_tensor)
-
-    # train_tensor_size = int(len(input_tensor) * 0.7)
-    # val_tensor_size = len(input_tensor) - train_tensor_size
-    # seed = torch.Generator().manual_seed(584)
-    # train_set, val_set = torch.utils.data.random_split(dataset, [train_tensor_size, val_tensor_size], generator=seed)
-
-
-    # train_dataloader = DataLoader(train_set,batch_size=200)
-    # val_dataloader = DataLoader(val_set,batch_size=200)
+    train_dataloader = DataLoader(train_dataset,batch_size = 64)
+    val_dataloader = DataLoader(val_Dataset,batch_size = 64)
     model = BasicLightning()
     trainer = pl.Trainer(
-        max_epochs=50000,
+        max_epochs=200000,
         devices="auto",
         accelerator="auto",
         strategy=ray.train.lightning.RayDDPStrategy(),
-        plugins=[ray.train.lightning.RayLightningEnvironment()],
-        callbacks=[ray.train.lightning.RayTrainReportCallback()],
+        plugins=[ray.train.lightning.RayLightningEnvironment()]
     )
     trainer = ray.train.lightning.prepare_trainer(trainer)
     trainer.fit(model, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader)
