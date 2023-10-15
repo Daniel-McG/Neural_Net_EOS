b209d99e86ca2b0c3d938cf78093208e0bbbdd34
Daniel-McG <rileyday@tutanota.com>
2023-10-15 18:43:53 +0100

    updated results name, started pandas code

--- data_coallation_script.py
+++ data_coallation_script.py
@@ -1,9 +1,10 @@
 import os
-
-results_filename = "log_nvt.lammps"
+import pandas as pd
+results_filename = "nvtave.lammps"
 for (root,dirs,files) in os.walk(r"/home/daniel/LJ-2d-md-results", topdown=True):
     for filename in files:
         if filename == results_filename:
             path_to_results = os.path.join(root,filename)
-            # currently_open_file = open(file=files[0],mode="r")
-            print(path_to_results)
+            pd.read_csv(path_to_results,skiprows=[0],delimiter=" ")
+            # currently_open_file = open(file=path_to_results,mode="r")
+            # print(currently_open_file.read())
