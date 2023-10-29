5ad0454dfecce1352cda156e0163827e4e7b73f6
Daniel-McG <rileyday@tutanota.com>
2023-10-29 18:57:51 +0000

    Revised formulas

--- properties.py
+++ properties.py
@@ -9,7 +9,7 @@ def isochoric_heat_capacity(N,PE,T):
 
     squared_1 = np.mean(PE**2)
     squared_2 = np.mean(PE)**2
-    part1 = (Na/N)*((np.mean(squared_1 - squared_2))/(Kb*T**2))
+    part1 = (Na/N)*((squared_1 - squared_2)/(Kb*T**2))
     part2 = (Na/N)*(3/2)*(N*Kb)
     return part1 + part2
 
@@ -19,21 +19,20 @@ def isobaric_heat_capacity(E,N,P,V,T):
     E_PV = E + (P_average*V_average)
     squared_1 = np.mean(E_PV**2)
     squared_2 = np.mean(E_PV)**2
-    return (Na/N)*(1/(Kb*(T**2)))*(np.mean(squared_1 - squared_2))
+    return (Na/N)*(1/(Kb*(T**2)))*(squared_1 - squared_2)
 
 def thermal_expansion_coefficient(E,P,PE,T,V):
     P_average = np.mean(P)
     V_average = np.mean(V)
     E_PV = E + (P_average*V_average)
     E_PV_average = np.mean(E_PV)
-    PE_average= np.mean(PE)
-    return (1/(Kb*(T**2)*PE_average))*(np.mean((PE - PE_average)*(E_PV-E_PV_average)))
+    return (1/(Kb*(T**2)*V_average))*(np.mean((V - V_average)*(E_PV-E_PV_average)))
 
-def isothermal_compressibility(PE,T):
-    PE_average = np.mean(PE)
-    squared_1 = np.mean(PE**2)
-    squared_2 = PE_average**2
-    return (1/(PE_average*Kb*T))*(np.mean(squared_1-squared_2))
+def isothermal_compressibility(V,T):
+    V_average = np.mean(V)
+    squared_1 = np.mean(V**2)
+    squared_2 = V_average**2
+    return (1/(V_average*Kb*T))*(squared_1-squared_2)
 
 def thermal_pressure_coefficient(P,PE,rho,T):
     PE_average = np.mean(PE)
