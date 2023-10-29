8dd58a891aae747ce98c7a44a2676027dc87a6c0
Daniel-McG <rileyday@tutanota.com>
2023-10-29 18:58:54 +0000

    Added test case and data array assignment

--- data_coallation_script.py
+++ data_coallation_script.py
@@ -10,9 +10,9 @@ def script(path_to_results):
     Calculates derivative properties for many LAMMPS runs by iterating through the results directory,
     calculating the derivative properties for each run then appending them to the results array. 
     '''
-    results_filename = "nvtave.lammps"
+    NVT_results_filename = "nvtave.lammps"
     column_names=["TimeStep","v_TENE","v_TEMP","v_PRES","v_DENS","v_nMolecule","v_KENE","v_PENE","v_ENTH","v_VOL"]
-
+    #               0           1       2       3           4           5           6       7       8       9
     # Creating numpy arrays with the correct dimensions to append the results to
     array_size = (1,12)
     coallated_means = np.zeros(array_size)
@@ -21,7 +21,7 @@ def script(path_to_results):
 
     for (root,dirs,files) in os.walk(path_to_results, topdown=True):
         for filename in files:
-            if filename == results_filename:
+            if filename == NVT_results_filename:
 
                 # Separate the folder structure into individual items in a list
                 split_root_folder_structure = str.split(root,sep="/")
@@ -33,7 +33,6 @@ def script(path_to_results):
                 temp_and_density = re.findall("\d+\.\d+", temp_and_density_foldername)
 
                 # Unpack the temperature and density list into the temperature and density variables
-
                 temperature, density = temp_and_density
 
                 # Convert temperature and density stings to floats
@@ -44,9 +43,33 @@ def script(path_to_results):
                 path_to_results = os.path.join(root,filename)
 
                 # Read the data and convert to numpy array
-                data_df = pd.read_csv(path_to_results,skiprows=[0,1],delimiter=" ",names=column_names)
+                data_df = pd.read_csv(path_to_results,
+                                      skiprows=[0,1],
+                                      delimiter=" ",
+                                      names=column_names
+                                      )
                 data_arr= data_df.to_numpy()
-                
+
+                # Assign columns of data to the respective variable
+                total_energy = data_arr[:,1]
+                pressure = data_arr[:,3]
+                number_of_particles = data_arr[:,5]
+                kinetic_energy = data_arr[:,6]
+                potential_energy = data_arr[:,7]
+                enthalpy = data_arr[:,8]
+                volume = data_arr[:,9]
+
+                # Test that the data has been read correctly 
+                if (temperature == 0.406597) & (density == 0.760723):
+                    assert total_energy[0] == -2.01527, "Data read failure , Total energy has been read incorrectly as per test case"
+                    assert pressure[0] == -0.0152766, "Data read failure , Pressure has been read incorrectly as per test case"
+                    assert number_of_particles[0] == 2048, "Data read failure , Number of particles has been read incorrectly as per test case"
+                    assert kinetic_energy[0] ==  0.405846, "Data read failure , Kinetic energy has been read incorrectly as per test case"
+                    assert potential_energy[0] == -2.42111, "Data read failure , Potential energy has been read incorrectly as per test case"
+                    assert enthalpy[0] == -2.03535, "Data read failure , Enthalpy has been read incorrectly as per test case"
+                    assert volume[0] == 2692.18, "Data read failure , Volume has been read incorrectly as per test case"
+
+                print(total_energy)
                 # mean_values = np.mean(arr,axis=0)
                 # means = np.append(mean_values,[temperature,density])
                 # standard_deviations = data.std().values
@@ -72,4 +95,4 @@ def script(path_to_results):
                 
     # means_df = pd.DataFrame(coallated_means,columns=column_names+["Temperature","Density"])
     # np.savetxt("coallated_results.txt",means_df.values)
-script()
+script(path_to_results)
