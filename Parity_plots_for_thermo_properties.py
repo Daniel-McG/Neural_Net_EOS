42b94865bbe390a32e6e67653766c7fb58692abf
Daniel-McG <rileyday@tutanota.com>
2023-11-27 16:34:08 +0000

    updated

--- Parity_plots_for_thermo_properties.py
+++ Parity_plots_for_thermo_properties.py
@@ -18,8 +18,14 @@ mu_jt_column = betaT_column + 1
 Z_column = mu_jt_column+1
 cv_column = Z_column+1
 gammaV_column = cv_column+1
-print(pd.DataFrame(arr[:,gammaV_column]).describe())
-print(pd.DataFrame(arr[:,alphaP_column].flatten()/arr[:,betaT_column].flatten()).describe())
+non_nan_alphaP = ~np.isnan(arr[:,alphaP_column])
+# print(arr[:,betaT_column])
+# arr[:,alphaP_column] = arr[:,alphaP_column]
+# # print(debug_arr[:,alphaP_column].flatten()/debug_arr[:,betaT_column].flatten())
+# print(pd.DataFrame(arr[:,gammaV_column]).describe())
+# print(pd.DataFrame(arr[:,alphaP_column]==).value_counts(ascending=False))
+# print(pd.DataFrame(arr[:,betaT_column]).describe())
+# print(pd.DataFrame(arr[:,alphaP_column].flatten()/arr[:,betaT_column].flatten()).describe())
 # non_zero_betaT_index = np.where(arr[:,betaT_column]!=0)
 
 # gammaV_from_data_coallation = arr[non_zero_betaT_index,gammaV_column].flatten()
@@ -27,17 +33,17 @@ print(pd.DataFrame(arr[:,alphaP_column].flatten()/arr[:,betaT_column].flatten())
 
 # # indices = np.where(np.logical_and(np.isfinite(gammaV_from_thermo_relation), gammaV_from_thermo_relation != np.inf, gammaV_from_thermo_relation != -np.inf))
 # # indicies = 1
-# # gammaV_from_data_coallation_debug = debug_arr[:,gammaV_column]
-# # gammaV_from_thermo_relation_debug = debug_arr[:,alphaP_column]/debug_arr[:,betaT_column]
+gammaV_from_data_coallation = debug_arr[non_nan_alphaP,gammaV_column].flatten()
+gammaV_from_thermo_relation = debug_arr[non_nan_alphaP,alphaP_column].flatten()/debug_arr[non_nan_alphaP,betaT_column].flatten()
 
 
 
-# cp_from_thermo_relation = arr[non_zero_betaT_index,cv_column].flatten() + (arr[non_zero_betaT_index,temperature_column].flatten()/arr[non_zero_betaT_index,density_column].flatten())*((arr[non_zero_betaT_index,alphaP_column].flatten()**2)/arr[non_zero_betaT_index,betaT_column].flatten())
+cp_from_thermo_relation = arr[:,cv_column].flatten() + (arr[:,temperature_column].flatten()/arr[:,density_column].flatten())*((arr[:,alphaP_column].flatten()**2)/arr[:,betaT_column].flatten())
 # # cp_from_thermo_relation2 = arr[:,cv_column] + (arr[:,temperature_column]/arr[:,density_column])*(arr[:,alphaP_column]*arr[:,gammaV_column])
 
 # # print(pd.DataFrame(np.abs((arr[:,alphaP_column]**2)/arr[:,betaT_column])<1e-20).describe())
 
-# cp_from_data_coallation = arr[non_zero_betaT_index,cp_column].flatten()
+cp_from_data_coallation = arr[:,cp_column].flatten()
 # # print((cp_from_thermo_relation - cp_from_data_coallation)<1e-5)
 # # cp_from_thermo_relation_debug = debug_arr[:,cv_column] + (debug_arr[:,temperature_column]/debug_arr[:,density_column])*((debug_arr[:,alphaP_column]**2)/debug_arr[:,betaT_column])
 # # cp_from_data_coallation_debug = debug_arr[:,cp_column]
@@ -52,24 +58,24 @@ print(pd.DataFrame(arr[:,alphaP_column].flatten()/arr[:,betaT_column].flatten())
 # # print(pd.DataFrame(np.abs(((arr[:,alphaP_column]/arr[:,betaT_column])- (arr[:,gammaV_column])))<1e-1).value_counts())
 # # sns.histplot(data = arr[non_zero_betaT_index,alphaP_column]/arr[non_zero_betaT_index,betaT_column])
 # # plt.show()
-# sns.scatterplot(x=gammaV_from_data_coallation,y=gammaV_from_thermo_relation,color ="r" )
-# # sns.scatterplot(x = gammaV_from_data_coallation_debug,y=gammaV_from_thermo_relation_debug,color="c")
-# plt.xlabel(r"$\gamma_V$ from MD")
-# plt.ylabel(r"$\gamma_V$ from $\alpha_P$ / $\beta_T$")
-# # sns.lineplot(x=[-0.001,0.0025],y=[-0.001,0.0025])
-# # plt.xlim(-0.001,0.0025)
-# # plt.ylim(-0.001,0.0025)
-# plt.show()
+sns.scatterplot(x=gammaV_from_data_coallation,y=gammaV_from_thermo_relation,color ="r" )
+# sns.scatterplot(x = gammaV_from_data_coallation_debug,y=gammaV_from_thermo_relation_debug,color="c")
+plt.xlabel(r"$\gamma_V$ from MD")
+plt.ylabel(r"$\gamma_V$ from $\alpha_P$ / $\beta_T$")
+sns.lineplot(x=[0,7],y=[0,7])
+# plt.xlim(-0.001,0.0025)
+# plt.ylim(-0.001,0.0025)
+plt.show()
 
-# sns.scatterplot(x=cp_from_data_coallation,y=cp_from_thermo_relation,color ="r" )
+sns.scatterplot(x=cp_from_data_coallation,y=cp_from_thermo_relation,color ="r" )
 # # sns.scatterplot(x = cp_from_data_coallation_debug,y=cp_from_thermo_relation_debug,color="c")
-# plt.xlabel(r"Cp from MD")
-# plt.ylabel(r"Cp from relation")
-# range = [0,60]
-# sns.lineplot(x=range,y=range)
-# plt.xlim(range)
-# plt.ylim(range)
-# plt.show()
+plt.xlabel(r"Cp from MD")
+plt.ylabel(r"Cp from relation")
+range = [0,60]
+sns.lineplot(x=range,y=range)
+plt.xlim(range)
+plt.ylim(range)
+plt.show()
 
 # sns.scatterplot(x=Z_from_data_coallation,y=Z_from_thermo_relation,color ="r" )
 # # sns.scatterplot(x = cp_from_data_coallation_debug,y=cp_from_thermo_relation_debug,color="c")
